62f02cadbc07a31976c13104d4dfd9da43ac52fb
vighindia <113588905+vighindia@users.noreply.github.com>
2024-11-05 14:10:41 +0530

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -21,8 +21,8 @@ public class TestUtil extends TestBase {
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	public static String TESTDATA_SHEET_PATH = "/Users/naveenkhunteta/Documents/workspace"
-			+ "/FreeCRMTest/src/main/java/com/crm/qa/testdata/FreeCrmTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "/Users/VKPRABHU/FreeCRMTest/src/main"
+			+ "/java/com/crm/qa/testdata/freecrm.xlsx";
 
 	static Workbook book;
 	static Sheet sheet;
