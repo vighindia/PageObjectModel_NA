5cc8a736982f0cd3242f4aba5d090e211081fa43
vighindia <113588905+vighindia@users.noreply.github.com>
2024-11-05 14:19:48 +0530

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -11,17 +11,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
+//import org.openqa.selenium.support.events.EventFiringWebDriver;
 
-import com.crm.qa.util.TestUtil;
-import com.crm.qa.util.WebEventListener;
+//import com.crm.qa.util.TestUtil;
+//import com.crm.qa.util.WebEventListener;
 
 public class TestBase {
 	
 	public static WebDriver driver;
 	public static Properties prop;
-	public  static EventFiringWebDriver e_driver;
-	public static WebEventListener eventListener;
+	//public  static EventFiringWebDriver e_driver;
+	//public static WebEventListener eventListener;
 	
 	public TestBase(){
 		try {
@@ -49,11 +49,11 @@ public class TestBase {
 		}
 		
 		
-		e_driver = new EventFiringWebDriver(driver);
+		//e_driver = new EventFiringWebDriver(driver);
 		// Now create object of EventListerHandler to register it with EventFiringWebDriver
-		eventListener = new WebEventListener();
-		e_driver.register(eventListener);
-		driver = e_driver;
+		//eventListener = new WebEventListener();
+		//e_driver.register(eventListener);
+		//driver = e_driver;
 		
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
